1dfb75dfb685fe4a394c3047bf51e0aad7621133
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-11-07 21:51:08 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan58-TambahKurang/src/if1/pkg10118008/latihan58/tambahkurang/IF110118008Latihan58TambahKurang.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ASUS
+ */
+public class IF110118008Latihan58TambahKurang {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		JumlahBilangan jb = new JumlahBilangan();
+		jb.tampilHasilJumlah();
+		SelisihBilangan sb = new SelisihBilangan();
+		sb.tampilSelisih();
+	}
+	
+}
---
+++ IF1-10118008-Latihan58-TambahKurang/src/if1/pkg10118008/latihan58/tambahkurang/SelisihBilangan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SelisihBilangan extends Bilangan {
+	public void tampilSelisih(){
+		System.out.println("Hasil Selisih " + this.getX() + " - " + this.getY() + " = " + (this.getX()-this.getY()));
+	}
+	
+}
